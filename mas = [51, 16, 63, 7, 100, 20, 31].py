a5dc8e368d64bae6d13c5372e89f8a8140b250b9
viktoriiarmnts <viktoriaromanets28@gmail.com>
2025-09-03 09:10:23 +0300

    Add files via upload

---
+++ mas = [51, 16, 63, 7, 100, 20, 31].py
@@ -0,0 +1,32 @@
+years_old = 16
+portion = 3.5
+firstname = 'Viktoriia'
+dataset = [5, 6, 7, 8]
+history = (True,)
+prices = {25, 30}
+index = 42
+duration = (2, 15)
+note = {'2': 'Storm'}
+block = b"Hello"
+
+print(type(years_old))
+print(type(portion))
+print(type(firstname))
+print(type(dataset))
+print(type(history))
+print(type(prices))
+print(type(index))
+print(type(duration))
+print(type(note))
+print(type(block))
+
+print(years_old)
+print(portion)
+print(firstname)
+print(dataset)
+print(history)
+print(prices)
+print(index)
+print(duration)
+print(note)
+print(block)
